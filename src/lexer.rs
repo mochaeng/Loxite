4472b58b45726fff1af7b1b95cb28102d4d7b051
Luiz Campos <campos.antoonio@gmail.com>
2024-06-02 21:59:20 -0300

    feat: lexer finished

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,27 +1,33 @@
-use std::str::Chars;
+use std::collections::HashMap;
 
 use crate::error;
+use crate::expr::LiteralToken;
 use crate::token::Token;
 use crate::token::TokenType;
-use crate::token::TokenType::*;
 
 #[derive(Debug)]
-pub struct Lexer {
+pub struct Lexer<'a> {
+    /// If an error happens during lexing
+    pub had_error: bool,
     source: Vec<char>,
     tokens: Vec<Token>,
     start: usize,
     current: usize,
     line: usize,
+    keywords: HashMap<&'a str, TokenType>,
 }
 
-impl Lexer {
+impl<'a> Lexer<'a> {
     pub fn new(source: &String) -> Self {
+        let keywords = Lexer::get_keywords();
         Lexer {
             source: source.chars().collect(),
             tokens: vec![],
             start: 0,
             current: 0,
             line: 1,
+            had_error: false,
+            keywords,
         }
     }
 
@@ -31,42 +37,42 @@ impl Lexer {
             self.tokenize();
         }
         self.tokens.push(Token {
-            type_of: EOF,
+            token_type: TokenType::EOF,
             lexeme: "".to_string(),
+            literal: LiteralToken::Empty,
             line: self.line,
         });
+
         &self.tokens
     }
 
     fn tokenize(&mut self) {
-        let mut had_error = false;
-
         match self.advance() {
-            '(' => self.add_token(LeftParen),
-            ')' => self.add_token(RightBrace),
-            '{' => self.add_token(LeftBrace),
-            '}' => self.add_token(RightBrace),
-            ',' => self.add_token(Comma),
-            '.' => self.add_token(Dot),
-            '-' => self.add_token(Minus),
-            '+' => self.add_token(Plus),
-            ';' => self.add_token(Semicolon),
-            '*' => self.add_token(Star),
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
             '!' => match self.match_next_char('=') {
-                true => self.add_token(BangEqual),
-                false => self.add_token(Bang),
+                true => self.add_token(TokenType::BangEqual),
+                false => self.add_token(TokenType::Bang),
             },
             '=' => match self.match_next_char('=') {
-                true => self.add_token(EqualEqual),
-                false => self.add_token(Equal),
+                true => self.add_token(TokenType::EqualEqual),
+                false => self.add_token(TokenType::Equal),
             },
             '<' => match self.match_next_char('=') {
-                true => self.add_token(LessEqual),
-                false => self.add_token(Less),
+                true => self.add_token(TokenType::LessEqual),
+                false => self.add_token(TokenType::Less),
             },
             '>' => match self.match_next_char('=') {
-                true => self.add_token(GreaterEqual),
-                false => self.add_token(Greater),
+                true => self.add_token(TokenType::GreaterEqual),
+                false => self.add_token(TokenType::Greater),
             },
             '/' => match self.match_next_char('/') {
                 true => {
@@ -74,13 +80,19 @@ impl Lexer {
                         self.advance();
                     }
                 }
-                false => self.add_token(Slash),
+                false => self.add_token(TokenType::Slash),
             },
             ' ' | '\r' | '\t' => {}
             '\n' => self.line += 1,
-            _ => {
-                error::error(self.line, "Unexpected character.");
-                had_error = true;
+            '"' => self.string(),
+            '0'..='9' => self.number(),
+            ch => {
+                if self.is_alpha(ch) {
+                    self.identifier();
+                } else {
+                    error::error(self.line, "Unexpected character.");
+                    self.had_error = true;
+                }
             }
         }
     }
@@ -97,16 +109,83 @@ impl Lexer {
         true
     }
 
-    fn add_token(&mut self, type_of: TokenType) {
+    fn add_token(&mut self, token_type: TokenType) {
+        self.add_token_with_literal(token_type, LiteralToken::Empty);
+    }
+
+    fn add_token_with_literal(&mut self, token_type: TokenType, literal: LiteralToken) {
         let text = self.source[self.start..self.current]
             .iter()
             .collect::<String>();
 
         self.tokens.push(Token {
-            type_of,
+            token_type,
             lexeme: text,
+            literal,
             line: self.line,
         });
+    }
+
+    fn string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            error::error(self.line, "Unterminaded string");
+            self.had_error = true;
+            return;
+        }
+
+        self.advance();
+
+        let value = self.source[self.start + 1..self.current - 1]
+            .iter()
+            .collect::<String>();
+
+        let literal = LiteralToken::String(value);
+        self.add_token_with_literal(TokenType::String, literal);
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            self.advance();
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let value = self.source[self.start..self.current]
+            .iter()
+            .collect::<String>()
+            .parse::<f64>()
+            .unwrap();
+
+        let literal = LiteralToken::Number(value);
+        self.add_token_with_literal(TokenType::Number, literal);
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let text = self.source[self.start..self.current]
+            .iter()
+            .collect::<String>();
+
+        let token_type = match self.keywords.get(text.as_str()) {
+            Some(value) => *value,
+            None => TokenType::Identifier,
+        };
+
+        self.add_token(token_type);
     }
 
     fn advance(&mut self) -> char {
@@ -122,7 +201,49 @@ impl Lexer {
         self.source[self.current]
     }
 
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            return '\0';
+        }
+        return self.source[self.current + 1];
+    }
+
+    fn is_digit(&self, ch: char) -> bool {
+        ch >= '0' && ch <= '9'
+    }
+
+    fn is_alpha(&self, ch: char) -> bool {
+        (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
+    }
+
+    fn is_alpha_numeric(&self, ch: char) -> bool {
+        self.is_alpha(ch) || self.is_digit(ch)
+    }
+
     fn is_at_end(&self) -> bool {
         self.current >= self.source.len()
+    }
+
+    fn get_keywords() -> HashMap<&'a str, TokenType> {
+        let mut keywords: HashMap<&'a str, TokenType> = HashMap::new();
+
+        keywords.insert("and", TokenType::And);
+        keywords.insert("class", TokenType::Class);
+        keywords.insert("else", TokenType::Else);
+        keywords.insert("false", TokenType::False);
+        keywords.insert("for", TokenType::For);
+        keywords.insert("fun", TokenType::Fun);
+        keywords.insert("if", TokenType::If);
+        keywords.insert("nil", TokenType::Nil);
+        keywords.insert("or", TokenType::Or);
+        keywords.insert("print", TokenType::Print);
+        keywords.insert("return", TokenType::Return);
+        keywords.insert("super", TokenType::Super);
+        keywords.insert("this", TokenType::This);
+        keywords.insert("true", TokenType::True);
+        keywords.insert("var", TokenType::Var);
+        keywords.insert("while", TokenType::While);
+
+        keywords
     }
 }
